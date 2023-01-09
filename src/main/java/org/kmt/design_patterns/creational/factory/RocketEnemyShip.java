ce4cd0455cd0414520502113d4f0ccd315c1cc95
KMT <kumawat.aditya743@gmail.com>
2023-01-09 22:06:38 +0530

    Factory Pattern

---
+++ src/main/java/org/kmt/design_patterns/creational/factory/RocketEnemyShip.java
@@ -0,0 +1,10 @@
+package org.kmt.design_patterns.creational.factory;
+
+public class RocketEnemyShip extends EnemyShip {
+
+    public RocketEnemyShip(){
+        setName("Rocket Enemy Ship");
+        setDamage(10.0);
+    }
+
+}
